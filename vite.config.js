7d423791e8df16f56e504f5b29f8a13c08e8ff95
@swastik1456 <swastik@grktraining.com>
2025-03-13 23:10:23 +0530

    port number add in vite.config.js file

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,12 @@
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
+
 export default defineConfig({
   plugins: [
     tailwindcss(),
   ],
+  server: {
+    port: 5173, // Set the server port to 5173
+    host: true,  // Allows external access (useful for Docker)
+  },
 })
