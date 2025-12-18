883f5dff52018c2c3a75d50264e013b385f1bd7a
Swastik Pradhan <118344814+Swastik258@users.noreply.github.com>
2025-12-18 16:16:05 +0530

    Update allowedHosts in Vite configuration

--- vite.config.js
+++ vite.config.js
@@ -8,6 +8,8 @@ export default defineConfig({
   server: {
   port: 5173,
   host: '0.0.0.0',
-  allowedHosts: 'all',
+allowedHosts: [
+      'carbon-foot-print-production.up.railway.app',
+    ],
   },
 })
